1fc0e4cc8f759aab7851e04b2cb34966e070d269
VasylDrebot <drebotvasyl19960101@gmail.com>
2021-11-11 01:35:39 +0200

    Homework JS

---
+++ homework1/main.js
@@ -0,0 +1,222 @@
+// Task 1, 2, 3, 4
+
+let a = 'hello';
+console.log(a);
+// alert(a);
+document.write(a);
+
+a = 'hello again!';
+console.log(a);
+// alert(a);
+document.write(a);
+
+
+let b = 'owu';
+console.log(b);
+// alert(b);
+document.write(b);
+
+b = 'university';
+console.log(b);
+// alert(b);
+document.write(b);
+
+
+let c = 'com';
+console.log(c);
+// alert(c);
+document.write(c);
+
+c = 'de';
+console.log(c);
+// alert(c);
+document.write(c);
+
+
+let d = 'ua';
+console.log(d);
+// alert(d);
+document.write(d);
+
+d = 'es';
+console.log(d);
+// alert(d);
+document.write(d);
+
+
+let e = 1;
+console.log(e);
+// alert(e);
+document.write(e);
+
+e = 20;
+console.log(e);
+// alert(e);
+document.write(e);
+
+
+let f = 10;
+console.log(f);
+// alert(f);
+document.write(f);
+
+f = 56;
+console.log(f);
+// alert(f);
+document.write(f);
+
+
+let g = -999;
+console.log(g);
+// alert(g);
+document.write(g);
+
+g = 519;
+console.log(g);
+// alert(g);
+document.write(g);
+
+
+let h = 123;
+console.log(h);
+// alert(h);
+document.write(h);
+
+h = 17;
+console.log(h);
+// alert(h);
+document.write(h);
+
+
+let i = 3.14;
+console.log(i);
+// alert(i);
+document.write(i);
+
+i = 'a string';
+console.log(i);
+// alert(i);
+document.write(i);
+
+
+let j = 2.7;
+console.log(j);
+// alert(j);
+document.write(j);
+
+j = -218;
+console.log(j);
+// alert(j);
+document.write(j);
+
+
+let k = 16;
+console.log(k);
+// alert(k);
+document.write(k);
+
+k = false;
+console.log(k);
+// alert(k);
+document.write(k);
+
+
+let l = true;
+console.log(l);
+// alert(l);
+document.write(l);
+
+l = '279';
+console.log(l);
+// alert(l);
+document.write(l);
+
+
+let m = false;
+console.log(m);
+// alert(m);
+document.write(m);
+
+m = 285;
+console.log(m);
+// alert(m);
+document.write(m);
+
+
+// Task 5
+
+let lastName = 'Drebot ';
+let firstName = 'Vasyl ';
+let middleName = 'Mykolayovych'
+let person = lastName + firstName + middleName;
+console.log(person);
+// console.log(`${lastName} ${firstName} ${middleName}`)
+
+
+// Task 6 - 7
+let name = prompt('What is your name?');
+let middleName2 = prompt('What is your middle name?');
+let age = prompt('How old are you?');
+alert('Вітаю' + ' ' + name + ' ' + middleName2 + '. ' + 'Тобі' + ' ' + age + ' ' + 'років');
+
+// Task 8, 9
+let a1 = 100;
+console.log(typeof a1)
+
+let b1 = '100';
+console.log(typeof b1);
+
+let c1 = true;
+console.log(typeof c1);
+
+
+// Task 10 - 22
+
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 >= 6);
+console.log(5 === 6);
+console.log(10 === 10);
+console.log(10 == 10);
+console.log(10 != 10);
+console.log(10 !== 10);
+console.log(10 > 10);
+console.log(123 === '123');
+console.log(123 == '123');
+
+
+// Task 24 - 30
+
+let str = "20";
+let a2 = 5;
+
+// 205 - a2 приводиться до стрічкового типу даних, відбувається конкатенація
+document.write(str + a2 + "<br/>");
+
+// Стрічку не можна відняти одну від одної, помножити або поділити,
+// щоб виконалось віднімання, множення, ділення, str приводиться до числового типу даних,
+// якщо цього не відбувається то отримуємо результат NaN
+// 15
+document.write(str - a2 + "<br/>");
+
+// 40
+document.write(str * "2" + "<br/>");
+
+// 10
+document.write(str / 2 + "<br/>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
